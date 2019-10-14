3b336500bb92b21fa9ebbe3d4891f595befeb64a
mibeldang <mibeldangculos@gmail.com>
2019-10-14 21:21:53 +0800

    Practice node

--- Prac.js
+++ Prac.js
@@ -1,19 +1,121 @@
-var http=require('http');
-var fs =require('fs');
-var filename ='prac.html';
-var html='<body background=" https://img.freepik.com/free-photo/green-texture_1160-888.jpg?size=626&ext=jpg"> <center> <h1><b>Practice Makes Perfect</b></h1></center></body>';
+var http = require('http');
+var fs = require('fs');
+var filename = 'prac.html';
+var html = '<body background=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh83HIJ_D7j-bPM5-THdnepAJfT1-dTVkvFcE5K2Tsui6Ghcv1"> <center><br></br><h1 style="font-family:Impact; font-size: 50px;color:green"> NodeJs Practice Connected to MongoDb </h1><br></br><hr style=" width:100%;height:5px;color:#919090; background-color:#919090"></hr> <br></br><p1  style="font-family:Impact; font-size: 25px"> To create a database in MongoDB, start by creating a MongoClient object, then specify a connection URL with the correct ip address and the name of the database you want to create.MongoDB will create the database if it does not exist, and make a connection to it.</p1><br></br><br></br><br></br><br></br> <div> <center> <b> Name: <b><input> </input> <br></br> <b>Address: <b><input> </input><center/></div></center></body>'
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
 
 
-fs.writeFile(filename,html ,(err)=>{
+fs.writeFile(filename, html, (err) => {
     if (err) throw Error;
-    console.log("Have been Saved...");
-});
+    console.log("Saveddd");
+})
 
-http.createServer((req,res) =>{
-    fs.readFile(filename,(error,data) =>{
-        res.writeHead('200', "{'Context-type': 'text/html'")
+http.createServer((req, res) => {
+    fs.readFile(filename, (error, data) => {
+        if (error) throw Error;
+        res.writeHead('200', "{'Context-type': 'text/html'");
         res.write(data);
         res.end();
-    });
+    })
+
+
 }).listen(8080);
+
+//Creating a database
+MongoClient.connect(url, (err, database) => {
+    if (err) throw Error;
+    console.log("Created a database");
+    database.close();
+})
+
+//Creating a collection of Students
+MongoClient.connect(url, (err, database) => {
+    if (err) throw Error;
+    var dbo = database.db("mydb");
+    dbo.createCollection("Students", (err, res) => {
+        if (err) throw Error;
+        console.log("Collection has been created..." + res);
+        database.close();
+    })
+})
+
+//Inserting many documents in a collection of student
+MongoClient.connect(url, (err, database) => {
+    if (err) throw Error;
+    var dbo = database.db("mydb");
+    var myObj = [
+        { name: "Luzmibel", address: "Santander, Cebu" },
+        { name: "Shadklyn", address: "Lapu Lapu City" },
+        { name: "Mae Luzyl", address: "Jimalalud Orriental" },
+        { name: "Dale Marck", address: "Santander, Cebu" },
+        { name: "Charles", address: "Carcar, Cebu" },
+        { name: "Vincent", address: "Dalaguete, Cebu" },
+        { name: "Melben", address: "Malabago, Negros" },
+        { name: "Mellalie", address: "Talamban, Cebu" }
+    ];
+    dbo.collection("Students").insertMany(myObj, (err, res) => {
+        if (err) throw Error;
+        console.log("Number of document inserted:  " + res.insertedCount);
+        database.close();
+    })
+})
+
+//Finding one documents in the collection of Students
+MongoClient.connect(url, (err, database) => {
+    if (err) throw Error;
+    var dbo = database.db("mydb");
+    dbo.collection("Students").findOne({}, (err, result) => {
+        if (err) throw Error;
+        console.log(" First document seen: "+ result.name);
+        database.close();
+    })  
+})
+
+//Find all documents in the collection of Students
+MongoClient.connect(url, (err, database) => {
+    if (err) throw Error;
+    var dbo = database.db("mydb");
+    dbo.collection("Students").find({}).toArray((err, res) => {
+        if (err) throw Error;
+        console.log("All documents are: "+ res.insertedCount );
+        database.close();
+
+    })
+})
+
+// //FindSome
+MongoClient.connect(url, (err, database) => {
+    if (err) throw Error;
+    var dbo = database.db("mydb");
+    dbo.collection("Students").find({}, { projection: { name: 1, address: 0} }).toArray((err, result) => {
+        if (err) throw Error;
+        console.log("FindSome result:" + result);
+        database.close();
+    })
+})
+
+//Finding documents with address of santander using query
+MongoClient.connect(url,(err,database)=>{
+    if(err) throw Error;
+    var dbo=database.db("mydb");
+    var query={address: "Santander,Cebu"};
+    dbo.collection("Students").find(query).toArray((err,result)=>{
+        if (err) throw Error;
+        console.log(result);
+        database.close();
+    })
+})
+
+MongoClient.connect(url,(err,database)=>{
+    if (err) throw Error;
+    var dbo=database.db("mydb");
+    var isort={name:1};
+    dbo.collection("Students").find().sort(isort).toArray((err,result)=>{
+        if (err) throw Error;
+        console.log(result);
+        database.close();
+    })
+
+})
 
